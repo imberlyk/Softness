38de3f2ece4c19c5afe08a835649b22a270a888b
Kim <77022750+imberlyk@users.noreply.github.com>
2024-04-16 13:14:54 -0400

    jpsdfgjs

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-$(document).ready(function(){
-  // Function for accordion
-  $('.accordion-title').click(function(){
-    $(this).parent().toggleClass('active');
-    $(this).next('.accordion-content').slideToggle();
-    $(this).find('.accordion-sign').text($(this).parent().hasClass('active') ? '-' : '+');
-  });
-  
-  // Function for school of glimmers
-  $("#school-of-glimmers").addClass("grow");
-});
+VANTA.CLOUDS({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  skyColor: 0xc7ddf7,
+  cloudShadowColor: 0x295582
+})
